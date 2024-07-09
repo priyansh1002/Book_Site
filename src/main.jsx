168cc8e4f0cc8f78a99e22289d6f004995cf8dbb
Priyansh <priyansh2001singh@gmail.com>
2024-07-09 11:24:50 +0530

    Book Search

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,4 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-// import Home from "./pages/Home/Home.jsx"
-// import About from "./pages/About/About.jsx"
-// import BookList from './components/BookList/BookList.jsx'
-// import BookDetails from './components/BookDetails/BookDetails.jsx'
-// import { AppProvider } from './context.jsx'
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//     <Route path='/' element={<Home />}>
-//       <Route path='about' element={<About />} />
-//       <Route path='book' element={<BookList />} />
-//       <Route path='/book/:id' element={<BookDetails />} />
-//     </Route>
-//     </>
-//   )
-// )
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <AppProvider>
-//     <RouterProvider router={router} />,
-//   </AppProvider>
-// )
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
